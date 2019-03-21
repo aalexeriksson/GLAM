051690760d516d75cf471bc65437c78de3bd1f25
Alexander Eriksson <aalex.e@hotmail.com>
2019-03-21 10:09:34 +0100

    database connection

--- src/main/java/com/example/demo/ReportedSuspicions.java
+++ src/main/java/com/example/demo/ReportedSuspicions.java
@@ -1,49 +1,19 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Service;
 
-
-@Service
 public class ReportedSuspicions {
     private long id;
-    private String category;
-    private String type;
+    private String crime;
 
-    public ReportedSuspicions(String category, String type) {
-        this.category = category;
-        this.type = type;
+    public ReportedSuspicions(String crime) {
+        this.crime = crime;
     }
 
-    public ReportedSuspicions() {
+    public String getCrime() {
+        return crime;
     }
 
-    public ReportedSuspicions(long id, String category, String type) {
-        this.id = id;
-        this.category = category;
-        this.type = type;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
+    public void setCrime(String crime) {
+        this.crime = crime;
     }
 }
