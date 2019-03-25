f64984e0a33925009b7f937425946cdd0516a264
Miriam Haile <miriam.haile@hotmail.com>
2019-03-25 10:30:35 +0100

    logo in tab

--- target/classes/static/script.js
+++ target/classes/static/script.js
@@ -112,23 +112,33 @@ var marker;
 navigator.geolocation.getCurrentPosition(success, failure);
 
 function success(position) {
+var currentDate = new Date();
+var time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+var date = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
     var myLat = position.coords.latitude;
     var myLong = position.coords.longitude;
+    position[0]=myLat;
+    position[1]=myLong;
 
-    var coords = new google.maps.LatLng(myLat, myLong);
+    var coords = new google.maps.LatLng(position[0], position[1]);
 
     var mapOptions = {
         zoom: 16,
-        center: coords,
+        center: coords
     }
-    document.getElementById('latitude').value = myLat;
-    document.getElementById('longitude').value = myLong;
+
+            document.getElementById('latitude').value = myLat;
+            document.getElementById('longitude').value = myLong;
+            document.getElementById('date').value = date;
+            document.getElementById('time').value = time;
+
 
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     var marker = new google.maps.Marker({
         animation: google.maps.Animation.DROP,
-        map: map, position: coords
+        map: map, 
+        position: coords
     });
 
 
@@ -156,6 +166,37 @@ function geocodeLatLng(geocoder, map, infowindow, marker) {
         });
       }
 
+      google.maps.event.addListener(map, 'click', function(event) {
+        var result = [event.latLng.lat(), event.latLng.lng()];
+        transition(result);
+        console.log(event.latLng.lat());
+    });
+    
+var numDeltas = 100;
+var delay = 10; //milliseconds
+var i = 0;
+var deltaLat;
+var deltaLng;
+
+function transition(result){
+    i = 0;
+    deltaLat = (result[0] - position[0])/numDeltas;
+    deltaLng = (result[1] - position[1])/numDeltas;
+    moveMarker();
+}
+
+function moveMarker(){
+    position[0] += deltaLat;
+    position[1] += deltaLng;
+    var latlng = new google.maps.LatLng(position[0], position[1]);
+    marker.setPosition(latlng);
+    if(i!=numDeltas){
+        i++;
+        setTimeout(moveMarker, delay);
+    }
+}
+
+
 }
 
 
@@ -166,6 +207,9 @@ function failure() {
 
 
 function initialize() {
+       var currentDate = new Date();
+        var time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+       var date = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
     var input = document.getElementById('location');
     var autocomplete = new google.maps.places.Autocomplete(input);
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
@@ -175,16 +219,20 @@ function initialize() {
 
         var myLat = place.geometry.location.lat();
         var myLong = place.geometry.location.lng();
-
+        position[0]=myLat;
+        position[1]=myLong;
+        
          document.getElementById('latitude').value = myLat;
          document.getElementById('longitude').value = myLong;
+         document.getElementById('date').value = date;
+         document.getElementById('time').value = time;
 
 
-        var coords = new google.maps.LatLng(myLat, myLong);
+         var coords = new google.maps.LatLng(position[0], position[1]);
 
         var mapOptions = {
             zoom: 16,
-            center: coords,
+            center: coords
         }
 
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -216,6 +264,47 @@ function geocodeLatLng(geocoder, map, infowindow, marker) {
           }
         });
       }
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        var result = [event.latLng.lat(), event.latLng.lng()];
+        transition(result);
+        console.log(event.latLng.lat());
+    }); 
+
+    var numDeltas = 100;
+    var delay = 10; //milliseconds
+    var i = 0;
+    var deltaLat;
+    var deltaLng;
+    
+    function transition(result){
+        i = 0;
+        deltaLat = (result[0] - position[0])/numDeltas;
+        deltaLng = (result[1] - position[1])/numDeltas;
+        moveMarker();
+    }
+    
+    function moveMarker(){
+        position[0] += deltaLat;
+        position[1] += deltaLng;
+        var latlng = new google.maps.LatLng(position[0], position[1]);
+        marker.setPosition(latlng);
+        if(i!=numDeltas){
+            i++;
+            setTimeout(moveMarker, delay);
+        }
+    }
+    
     });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+            var cloud = 'dfyxx5zdo'; // todo your cloud_name here!
+            var preset= 'qsqog8rz'; // todo your upload_preset here!
+
+            document.getElementById("upload").addEventListener("click", function() {
+                cloudinary.openUploadWidget({ cloud_name: cloud, upload_preset: preset, multiple: false, resource_type: 'image', folder: 'test', form: 'form', field_name: 'picturecloudinary', thumbnails: '#thumbnail'},
+                function(error, result) { console.log(error, result) });
+
+                console.log('i was clicked');
+            }, false);
