e373cd160d1896ee60cc2121569ae24c7864077f
Miriam Haile <miriam.haile@hotmail.com>
2019-03-27 15:15:32 +0100

    new folder for static

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -24,8 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/ChooseCategory", "/ObjectForm", "/PersonForm", "/ActivityForm","/form", "/init", "/h2", "/h2/**").permitAll()
-                .antMatchers("/TipReceiverHome","/police", "/receiver").hasRole("ADMIN")
+                .antMatchers("/newFolder/*","/", "/ChooseCategory", "/ObjectForm", "/PersonForm", "/ActivityForm","/form", "/init", "/h2", "/h2/**").permitAll()
+                .antMatchers("/PoliceProfile").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
